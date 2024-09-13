9a978d5994caeeee27d928b66349dcb8c9720fcf
Shankar <sankargv17999@gmail.com>
2024-09-13 11:46:36 +0530

    variables

---
+++ variables/ec2.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "allow_ssh_terraform" {
+  name        = var.sg_name
+  description = "Allow SSH access on port 22"
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1" # Allow all outbound traffic
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = var.from_port
+    to_port          = var.to_port
+    protocol         = var.protocal
+    cidr_blocks      = var.ingress_cidr # Consider restricting to specific IP range for better security
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = var.tags
+}
+
+resource "aws_instance" "terraform" {
+  ami                         =  var.ami_id
+  instance_type               = "t3.micro"
+  vpc_security_group_ids      = [aws_security_group.allow_ssh_terraform.id]
+
+  tags = var.tags
+}
---
+++ variables/variable.tf
@@ -0,0 +1,53 @@
+variable "ami_id" {
+  type        = string
+  default     = "ami-09c813fb71547fc4f"
+}
+
+
+variable "instance_type" {
+  type        = string
+  default     = "t3.micro"
+  
+}
+
+variable "sg_name"{
+ type = string
+ default ="allow_ssh"
+}
+
+
+
+variable "tags" {
+    type = map #optional
+    default = {
+        Name = "backend"
+        Project = "expense"
+        Component = "backend"
+        Environment = "DEV"
+        Terraform = "true"
+    }
+}
+
+
+variable "from_port"{
+    type = number
+    default =22
+}
+
+
+variable "to_port"{
+    type = number
+    default = 22
+}
+
+
+variable "protocal"{
+     
+    default ="tcp"
+}
+
+
+variable "ingress_cidr" {
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
