7b0b747ba9b36fa43b8131b566a96562266edf0e
Shankar <sankargv17999@gmail.com>
2024-09-13 12:18:48 +0530

    conditions

---
+++ conditions/variable.tf
@@ -0,0 +1,67 @@
+
+# 1. command line
+# 2. terraform.tfvars
+# 3. environment variables, TF_VAR_your_variable_name
+# 4. default
+# 5. prompt
+
+
+variable "ami_id" {
+  type        = string
+  default     = "ami-09c813fb71547fc4f"
+}
+
+
+# variable "instance_type" {
+#   type        = string
+#   default     = "t3.micro"
+  
+# }
+
+variable "environment"{
+  default = "prod"
+}
+
+variable "sg_name"{
+ type = string
+ default ="allow_ssh"
+}
+
+
+
+variable "tags" {
+    type = map #optional
+    default = {
+        Name = "backend"
+        Project = "expense"
+        Component = "backend"
+        Environment = "DEV"
+        Terraform = "true"
+    }
+}
+
+
+variable "from_port"{
+    type = number
+    default =22
+}
+
+
+variable "to_port"{
+    type = number
+    default = 22
+}
+
+
+variable "protocal"{
+     
+    default ="tcp"
+}
+
+
+variable "ingress_cidr" {
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
+
+
